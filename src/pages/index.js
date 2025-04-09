711be4a78c9239199a123ae9836d4087f71a9d92
nobuyukikobayashi <59198478+nbr41to@users.noreply.github.com>
2025-04-09 14:19:44 +0900

    Revert "Merge branch 'main' of https://github.com/nbr41to/pokepoke-friends"
    This reverts commit 29facd75da67c86e9629448afce0f40a71681ac1, reversing
    changes made to f6e53f4f6509923f0dc0cc5a890900d40afdb4d2.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,7 @@
-import styles from "@/styles/Home.module.css";
-import { Button } from "@/components/Button";
-import { useEffect, useState } from "react";
+import styles from '@/styles/Home.module.css';
+import { Button } from '@/components/Button';
 
 export default function Home() {
-  const [data, setData] = useState();
-
-  useEffect(() => {
-    const fetchData = async () => {
-  const response = await fetch("https://apex.oracle.com/pls/apex/gamelive/pokemon/", {
-    method: "GET",
-    headers: {
-      "Authorization": `Bearer ${yourApiKey}`, 
-    },
-  });
-  const result = await response.json();
-  setData(result.pokemon);
-      } catch (error) {
-        console.error("データ取得に失敗しました:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  console.log(data);
-
   return (
     <div>
       <header>
