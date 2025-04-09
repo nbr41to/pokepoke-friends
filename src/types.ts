c238f228e8f5a7a10ddd90dd3bce786810b88773
nobuyukikobayashi <59198478+nbr41to@users.noreply.github.com>
2025-04-09 22:59:54 +0900

    .

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,15 @@
+type Type =
+  | 'grass'
+  | 'fire'
+  | 'water'
+  | 'electric'
+  | 'fighting'
+  | 'psychic'
+  | 'darkness'
+  | 'metal'
+  | 'dragon'
+  | 'normal';
+
 type CardType =
   | 'pokemon'
   | 'trainer-goods'
@@ -9,21 +21,24 @@ type PokemonCard = {
   type: 'pokemon';
   name: string;
   image: string;
-  hp: number;
-  weakness: string;
-  ability1: string; // 技1の名前
+  pokemonType: Type; // ポケモンのタイプ
+  weakness: string; // 弱点タイプ
+  trait?: string; // 特性
+  abilityName1: string; // 技1の名前
   abilityDescription1: string; // 技1の説明
   abilityEnergy1: Record<string, number>; // 技1に必要なエネルギー
-  ability2?: string; // 技2の名前
+  abilityName2?: string; // 技2の名前
   abilityDescription2?: string; // 技2の説明
   abilityEnergy2?: Record<string, number>; // 技2に必要なエネルギー
-  trait?: string; // 特性
+  retreatCost: number; // にげるエネルギー
 };
 
 type TrainerCard = {
   id: string;
-  type: CardType;
+  type: Exclude<CardType, 'pokemon'>;
   name: string;
   image: string;
   description: string;
 };
+
+type Card = PokemonCard | TrainerCard;
--- tests/get-card-data.spec.ts
+++ tests/get-card-data.spec.ts
@@ -5,30 +5,38 @@ test('test', async ({ page }) => {
   const cardList = page.locator('.w-pkc-data-list-element');
 
   const cardData = await cardList.evaluateAll((elements) => {
-    console.log('elements', elements);
     return elements.map((element) => {
+      const id = element
+        .querySelector('._thumb icard')
+        ?.getAttribute('data-aid');
       const image = element.querySelector('._thumb img')?.getAttribute('src');
-      console.log('image', image);
       const body = element.querySelector('._body');
       const name = body?.querySelector('._name a')?.textContent;
       const hp = body?.querySelector('._hp ._v')?.textContent;
-      const type = body?.querySelector('._tag ._icon')?.className;
-      const context = body?.querySelector('._context ._column-table');
-      const abilityName = context?.querySelector('card')?.textContent;
-      const abilityText = context
-        ?.querySelector('card')
-        ?.getAttribute('data-txt');
-      const abilityPower = context?.querySelector('_val')?.textContent;
+      const type = body
+        ?.querySelector('._icon')
+        ?.className.replace(/_icon _icon- /, '');
+      const contexts = body?.querySelectorAll('._context ._column-table');
+      const quantityText = contexts?.[0]?.querySelector('.req')?.textContent;
+      const quantity = quantityText?.replace('✕', '');
+      const costType = contexts?.[0]?.querySelector('.req')?.textContent;
 
       return {
+        id,
         image,
         name,
         hp,
         type,
         ability1: {
-          name: abilityName,
-          text: abilityText,
-          power: abilityPower,
+          name: contexts?.[0]?.querySelector('card')?.textContent,
+          text: contexts?.[0]?.querySelector('card')?.getAttribute('data-txt'),
+          power: contexts?.[0]?.querySelector('._val')?.textContent,
+          costs: contexts?.[0]?.querySelector('.req')?.textContent,
+        },
+        ability2: {
+          name: contexts?.[1]?.querySelector('card')?.textContent,
+          text: contexts?.[1]?.querySelector('card')?.getAttribute('data-txt'),
+          power: contexts?.[1]?.querySelector('._val')?.textContent,
         },
       };
     });
