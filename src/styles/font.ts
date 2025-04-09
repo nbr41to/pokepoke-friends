1eccd98eee06db8e3d1f0814ec5718de1a79c06e
nobuyukikobayashi <59198478+nbr41to@users.noreply.github.com>
2025-04-09 14:20:32 +0900

    Revert "yuki-spetial"
    This reverts commit a7c0cf5431a65a8a8cd9be8bddebb389be8061b8.

---
+++ src/styles/font.ts
@@ -0,0 +1,8 @@
+import { Hachi_Maru_Pop } from 'next/font/google';
+
+export const hachiMaruPop = Hachi_Maru_Pop({
+  variable: '--font-noto-sans-jp',
+  weight: ['400'],
+  subsets: ['latin'],
+  display: 'swap',
+});
