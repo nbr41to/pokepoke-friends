8d4b2ce8db1b305645bb1f757a5290e5fa52b2ee
nobuyukikobayashi <59198478+nbr41to@users.noreply.github.com>
2025-05-12 11:57:51 +0900

    list viewのパフォーマンスを向上

--- src/app/search/_components/search-results/card-list.tsx
+++ src/app/search/_components/search-results/card-list.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import type { Card } from '@/generated/prisma';
+import { useEffect, useRef, useState } from 'react';
 import { CardListItem } from './card-list-item';
 
 type Props = {
@@ -10,8 +11,44 @@ export const CardList = ({ cards }: Props) => {
   return (
     <div className="divide-y divide-blue-300">
       {cards.map((card) => (
-        <CardListItem key={card.numbering} card={card} />
+        <LazyCardListItem key={card.numbering} card={card} />
       ))}
+    </div>
+  );
+};
+
+type LazyCardListItemProps = {
+  card: Card;
+};
+
+const LazyCardListItem = ({ card }: LazyCardListItemProps) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect();
+        }
+      },
+      {
+        rootMargin: '240px', // 画面外240pxの位置からロードを開始
+        threshold: 0.1,
+      },
+    );
+
+    observer.observe(ref.current);
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <div className="h-[102px]" ref={ref}>
+      {isVisible ? <CardListItem card={card} /> : <></>}
     </div>
   );
 };
