a0456f3cc2c92be78fde35d9233e536c3eb8b83e
nobuyukikobayashi <59198478+nbr41to@users.noreply.github.com>
2025-06-27 23:08:01 +0900

    Hotfix

--- src/app/(authenticated)/decks/[id]/edit/_components/deck-cards-form.tsx
+++ src/app/(authenticated)/decks/[id]/edit/_components/deck-cards-form.tsx
@@ -1,5 +1,5 @@
-import { ResetButton } from '@/components/search-form';
 import { KeywordDebounceForm } from '@/components/search-form/keyword-debounce-form';
+import { ResetIconButton } from '@/components/search-form/reset-icon-button';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { DeckCardsFilter } from './deck-cards-filter';
@@ -18,9 +18,9 @@ export const DeckCardsForm = ({
   onClearDeck,
 }: Props) => {
   return (
-    <div className="space-y-3 bg-white p-4 shadow">
-      <div className="sticky top-0 z-10 space-y-3 bg-white/90 p-3">
-        <div className="flex items-center justify-between">
+    <div className="space-y-3 bg-white shadow">
+      <div className="font-hachiMaru sticky top-0 z-10 w-screen space-y-3 bg-white/80 px-2 py-3">
+        <div className="flex items-center gap-x-3">
           <Button
             variant="destructive"
             className="font-bold"
@@ -28,32 +28,39 @@ export const DeckCardsForm = ({
           >
             はじめから
           </Button>
-          <p className="">{selectedCardIds.length} / 20</p>
+          <p className="">{selectedCardIds.length} / 20 枚</p>
         </div>
-        <div className="flex flex-wrap gap-1 rounded border p-2">
-          {selectedCardIds.map((cardId, index) => (
-            <button
-              // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
-              key={cardId + index}
-              type="button"
-              onClick={() => onCardDeselect(cardId)}
-            >
-              <Image
-                src={`https://pkhquuguchymresgmtey.supabase.co/storage/v1/object/public/card-images/${cardId.replace('#', '_')}.png`}
-                alt={cardId}
-                width={112}
-                height={112}
-              />
-            </button>
-          ))}
+        <div className="w-full overflow-x-scroll rounded border">
+          <div className="grid h-[212px] w-max grid-cols-10 gap-1 overflow-x-scroll p-2">
+            {selectedCardIds.map((cardId, index) => (
+              <button
+                // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                key={cardId + index}
+                type="button"
+                className="min-w-[68px]"
+                onClick={() => onCardDeselect(cardId)}
+              >
+                <Image
+                  src={`https://pkhquuguchymresgmtey.supabase.co/storage/v1/object/public/card-images/${cardId.replace('#', '_')}.png`}
+                  alt={cardId}
+                  width={68}
+                  height={68}
+                />
+              </button>
+            ))}
+          </div>
         </div>
-        <div className="flex flex-col items-center gap-2 md:flex-row">
+
+        <div className="mx-auto flex max-w-80 items-center justify-end gap-2">
           <KeywordDebounceForm />
-          <ResetButton />
+          <ResetIconButton />
         </div>
       </div>
 
-      <DeckCardsFilter onCardSelect={onCardSelect} />
+      <DeckCardsFilter
+        selectedCardIds={selectedCardIds}
+        onCardSelect={onCardSelect}
+      />
     </div>
   );
 };
