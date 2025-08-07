5d39c2247751d607715701953bc2908dcd247a42
nobuyukikobayashi <59198478+nbr41to@users.noreply.github.com>
2025-08-07 12:14:26 +0900

    タイプの選択のUIを変更

--- src/components/search-form/pokemon-type-form.tsx
+++ src/components/search-form/pokemon-type-form.tsx
@@ -1,15 +1,8 @@
 'use client';
 
-import { Button } from '@/components/ui/button';
-import { ToggleGroup, ToggleGroupItem } from '@/components/ui/toggle-group';
-import {
-  POKEMON_TYPE_LIST,
-  type PokemonType,
-} from '@/constants/types/pokemon-types';
-import { cn } from '@/utils/classnames';
+import type { PokemonType } from '@/constants/types/pokemon-types';
 import { useSearchQuery } from '@/utils/use-search-query';
-import { X } from 'lucide-react';
-import Image from 'next/image';
+import { MultiSelectPokemonType } from '../multi-select-pokemon-type';
 
 export const PokemonTypeForm = () => {
   const { query, setQuery } = useSearchQuery();
@@ -17,47 +10,13 @@ export const PokemonTypeForm = () => {
   const handleOnValueChange = (value: string[]) => {
     setQuery({ ...query, pokemonTypes: value as PokemonType[] });
   };
-  const isCleared = query.pokemonTypes.length === 0;
 
   return (
-    <div className="flex min-w-[402px] items-center gap-x-1">
-      <ToggleGroup
-        type="multiple"
-        value={query.pokemonTypes}
-        onValueChange={handleOnValueChange}
-        className="border"
-      >
-        {POKEMON_TYPE_LIST.map((type, index) => (
-          <ToggleGroupItem
-            key={type}
-            value={type}
-            aria-label={`Toggle ${type}`}
-            className="flex-none data-[state=off]:opacity-60 data-[state=off]:grayscale"
-          >
-            <Image
-              src={`/type${index + 1}.png`}
-              alt={`Type ${type}`}
-              width={20}
-              height={20}
-            />
-          </ToggleGroupItem>
-        ))}
-      </ToggleGroup>
-      <Button
-        size="icon"
-        variant="ghost"
-        onClick={() => {
-          setQuery({
-            ...query,
-            pokemonTypes: isCleared ? [...POKEMON_TYPE_LIST] : [],
-          });
-        }}
-      >
-        <span className={cn('text-gray-600', isCleared ? 'block' : 'hidden')}>
-          ALL
-        </span>
-        <X className={cn('size-5', isCleared ? 'hidden' : 'block')} />
-      </Button>
+    <div className="w-full">
+      <MultiSelectPokemonType
+        values={query.pokemonTypes}
+        onChange={handleOnValueChange}
+      />
     </div>
   );
 };
